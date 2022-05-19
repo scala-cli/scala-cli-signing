e1c582ec28b5bb276fff03be411182ea8d2f868d
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-05-19 19:16:15 +0200

    Add verbosity to pgp key-id command

--- cli-options/src/scala/cli/signing/commands/PgpKeyIdOptions.scala
+++ cli-options/src/scala/cli/signing/commands/PgpKeyIdOptions.scala
@@ -2,9 +2,16 @@ package scala.cli.signing.commands
 
 import caseapp._
 
+// format: off
 final case class PgpKeyIdOptions(
-  fingerprint: Boolean = false
-)
+  fingerprint: Boolean = false,
+  @ExtraName("v")
+    verbose: Int @@ Counter = Tag.of(0)
+) {
+  // format: on
+
+  lazy val verbosity = Tag.unwrap(verbose)
+}
 
 object PgpKeyIdOptions {
   implicit lazy val parser: Parser[PgpKeyIdOptions] = Parser.derive
